0d75dbf5e674ef2f872db319a76d8df9916c3019
Shalmalinighot <shalmali.nighot@gmail.com>
2023-11-27 19:42:43 +0530

    doctor-managementSystem has been uploaded

---
+++ Backend/das/das/src/main/java/com/abc/das/repository/IDoctorRepository.java
@@ -0,0 +1,24 @@
+package com.abc.das.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.abc.das.dto.DoctorDto;
+import com.abc.das.entity.Doctor;
+//import com.abc.das.entity.Patient;
+
+@Repository
+public interface IDoctorRepository extends JpaRepository<Doctor,Integer>{
+
+	Object save(DoctorDto doctor);
+	@Query("select d from Doctor d where d.email= :mail and d.password= :pwd")
+	Optional<Doctor>login(@Param("mail") String email,@Param("pwd") String password);
+    Optional<Doctor> findByemail(String email);
+List<Doctor> findByspecialization(String specialization);
+}
+
---
+++ Backend/das/das/src/main/java/com/abc/das/service/IAvailabilityService.java
@@ -0,0 +1,16 @@
+package com.abc.das.service;
+
+import java.util.List;
+
+import com.abc.das.dto.AvailabilityDto;
+import com.abc.das.entity.Availability;
+import com.abc.das.entity.Doctor;
+import com.abc.das.exception.DoctorNotFoundException;
+
+public interface IAvailabilityService {
+	public AvailabilityDto addAvailability(AvailabilityDto avlDto);
+	public Availability updateAvailability(Availability availability);
+	public void removeAvailability(int availabilityId);
+	List<Availability> getAvailabilityByDoctorId(int doctorId);
+	 public Availability getAvailabilityById(int id);
+}
